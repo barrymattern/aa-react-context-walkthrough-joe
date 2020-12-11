3a3e77ba351bdf9daefa8c0d6ab61a7b35dcd5d8
Barry Mattern <bdmattern@gmail.com>
2020-12-11 11:37:48 -0700

    Add NiceBox.js, get ready for Context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import PictureList from "./components/PictureList"
+import { useState } from "react";
+import NiceBox from "./components/NiceBox"
 
-const carImages = [ // info usually comes from your server
+function App() {
+  const [carImages, setCarImages] = useState([ // info usually comes from your server
   "https://cdn1.mecum.com/auctions/fl0117/fl0117-275088/images/fl0117-275088_1@2x.jpg?1481909731000",
   "https://cdn1.mecum.com/auctions/fl0117/fl0117-275088/images/fl0117-275088_2@2x.jpg?1481909731000",
   "https://cdn1.mecum.com/auctions/fl0117/fl0117-275088/images/fl0117-275088_8@2x.jpg?1481909731000",
   "https://cdn1.mecum.com/auctions/fl0117/fl0117-275088/images/fl0117-275088_3@2x.jpg?1481909731000",
-]
+]);
 
-function App() {
   return (
     <div id="app-element">
-      <PictureList imgUrls={carImages} />
+      <NiceBox />
     </div>
   );
 };
---
+++ src/components/NiceBox.js
@@ -0,0 +1,11 @@
+import PictureList from "./PictureList";
+
+const NiceBox = () => {
+  return (
+    <div className="nice-box">
+      <PictureList imgUrls={[]} />
+    </div>
+  );
+};
+
+export default NiceBox;
