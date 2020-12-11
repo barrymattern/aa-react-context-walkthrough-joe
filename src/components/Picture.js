883326cefa814b384cefd0f8c2f4b0d6e2f2e9a8
Barry Mattern <bdmattern@gmail.com>
2020-12-11 14:19:02 -0700

    Remove unnecessary comments

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,15 +1,12 @@
-import CarImagesContext from "../context/CarImagesContext";
 import { useContext } from "react";
+import CarImagesContext from "../context/CarImagesContext";
 
 const Picture = ({ imgUrl }) => { // props.imgUrl
   const valueFromAppJs = useContext(CarImagesContext);
   console.log(valueFromAppJs);
 
   return (
-    <img
-      src={imgUrl}
-      alt=""
-    />
+    <img src={imgUrl} alt="1969 DODGE HEMI SUPER BEE" />
   );
 };
 
--- src/components/PictureList.js
+++ src/components/PictureList.js
@@ -2,16 +2,14 @@ import Picture from './Picture';
 import { useContext } from 'react';
 import CarImagesContext from '../context/CarImagesContext';
 
-// Expect to receive a prop call "imgUrls"
-// which will be an array of strings that should be URLs
 const PictureList = () => {
   const sharedValueFromAppJs = useContext(CarImagesContext);
   const imgUrls = sharedValueFromAppJs.carUrls
 
   return (
     <ul>
-      {imgUrls.map((imageUrl, i) => {
-        return <li key={i}><Picture imgUrl={imageUrl} /></li>
+      {imgUrls.map((imgUrl, i) => {
+        return <li key={i}><Picture imgUrl={imgUrl} /></li>
       })}
     </ul>
   );
